931f4a10933271ae111798c139cb7aa870f3c8a5
jajjage <mustaphajajjage@gmail.com>
2024-11-21 12:01:39 +0100

    Feat: about page

--- src/hooks/groups/index.ts
+++ src/hooks/groups/index.ts
@@ -1,7 +1,8 @@
-import { onGetExploreGroup, onGetGroupInfo, onSearchGroups, onUpDateGroupSettings } from "@/actions/groups"
+import { onGetExploreGroup, onGetGroupInfo, onSearchGroups, onUpdateGroupGallery, onUpDateGroupSettings } from "@/actions/groups"
 import { GroupSettingsSchema } from "@/components/forms/group-settings/schema"
+import { UpdateGallerySchema } from "@/components/forms/media-gallery/schema"
 import { upload } from "@/lib/uploadcare"
-import { supabaseClient } from "@/lib/utils"
+import { supabaseClient, validateURLString } from "@/lib/utils"
 import { onClearList, onInfiniteScroll } from "@/redux/slices/infinite-scroll-slice"
 import { onOnline } from "@/redux/slices/online-member-slice"
 import { GroupStateProps, onClearSearch, onSearch } from "@/redux/slices/search-slice"
@@ -10,7 +11,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useMutation, useQuery } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 import { JSONContent } from "novel"
-import { useEffect, useLayoutEffect, useState } from "react"
+import { useEffect, useLayoutEffect, useRef, useState } from "react"
 import { useForm } from "react-hook-form"
 import { useDispatch } from "react-redux"
 import { toast } from "sonner"
@@ -315,4 +316,238 @@ export const useExploreSlider = (query: string, paginate: number) => {
   }, [])
 
   return { refetch, isFetching, data, onLoadSlider }
+}
+
+export const useGroupInfo = () => {
+  const { data } = useQuery({
+    queryKey: ["about-group-info"],
+  })
+
+  const router = useRouter()
+
+  if (!data) router.push("/explore")
+
+  const { group, status } = data as { status: number; group: GroupStateProps }
+
+  if (status !== 200) router.push("/explore")
+
+  return {
+    group,
+  }
+}
+
+export const useGroupAbout = (
+  description: string | null,
+  jsonDescription: string | null,
+  htmlDescription: string | null,
+  currentMedia: string,
+  groupid: string,
+) => {
+  const editor = useRef<HTMLFormElement | null>(null)
+  const mediaType = validateURLString(currentMedia)
+  const [activeMedia, setActiveMedia] = useState<
+    | {
+        url: string | undefined
+        type: string
+      }
+    | undefined
+  >(
+    mediaType.type === "IMAGE"
+      ? {
+          url: currentMedia,
+          type: mediaType.type,
+        }
+      : { ...mediaType },
+  )
+
+  const jsonContent =
+    jsonDescription !== null ? JSON.parse(jsonDescription as string) : undefined
+
+  const [onJsonDescription, setJsonDescription] = useState<
+    JSONContent | undefined
+  >(jsonContent)
+
+  const [onDescription, setOnDescription] = useState<string | undefined>(
+    description || undefined,
+  )
+
+  const [onHtmlDescription, setOnHtmlDescription] = useState<
+    string | undefined
+  >(htmlDescription || undefined)
+
+  const [onEditDescription, setOnEditDescription] = useState<boolean>(false)
+
+  const {
+    setValue,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<z.infer<typeof GroupSettingsSchema>>({
+    resolver: zodResolver(GroupSettingsSchema),
+  })
+
+  const onSetDescriptions = () => {
+    const JsonContent = JSON.stringify(onJsonDescription)
+    setValue("jsondescription", JsonContent)
+    setValue("description", onDescription)
+    setValue("htmldescription", onHtmlDescription)
+  }
+
+  useEffect(() => {
+    onSetDescriptions()
+    return () => {
+      onSetDescriptions()
+    }
+  }, [onJsonDescription, onDescription])
+
+  const onEditTextEditor = (event: Event) => {
+    if (editor.current) {
+      !editor.current.contains(event.target as Node | null)
+        ? setOnEditDescription(false)
+        : setOnEditDescription(true)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("click", onEditTextEditor, false)
+    return () => {
+      document.removeEventListener("click", onEditTextEditor, false)
+    }
+  }, [])
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["about-description"],
+    mutationFn: async (values: z.infer<typeof GroupSettingsSchema>) => {
+      if (values.description) {
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "DESCRIPTION",
+          values.description,
+          `/about/${groupid}`,
+        )
+        if (updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty",
+          })
+        }
+      }
+      if (values.jsondescription) {
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "JSONDESCRIPTION",
+          values.jsondescription,
+          `/about/${groupid}`,
+        )
+        if (updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty",
+          })
+        }
+      }
+      if (values.htmldescription) {
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "HTMLDESCRIPTION",
+          values.htmldescription,
+          `/about/${groupid}`,
+        )
+        if (updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty",
+          })
+        }
+      }
+      if (
+        !values.description &&
+        !values.jsondescription &&
+        !values.htmldescription
+      ) {
+        return toast("Error", {
+          description: "Oops! looks like your form is empty",
+        })
+      }
+      return toast("Success", {
+        description: "Group description updated",
+      })
+    },
+  })
+  const onSetActiveMedia = (media: { url: string | undefined; type: string }) =>
+    setActiveMedia(media)
+
+  const onUpdateDescription = handleSubmit(async (values) => {
+    mutate(values)
+  })
+
+  return {
+    setOnDescription,
+    onDescription,
+    setJsonDescription,
+    onJsonDescription,
+    errors,
+    onEditDescription,
+    editor,
+    activeMedia,
+    onSetActiveMedia,
+    setOnHtmlDescription,
+    onUpdateDescription,
+    isPending,
+  }
+}
+
+export const useMediaGallery = (groupid: string) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<z.infer<typeof UpdateGallerySchema>>({
+    resolver: zodResolver(UpdateGallerySchema),
+  })
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["update-gallery"],
+    mutationFn: async (values: z.infer<typeof UpdateGallerySchema>) => {
+      if (values.videourl) {
+        const update = await onUpdateGroupGallery(groupid, values.videourl)
+        if (update && update.status !== 200) {
+          return toast("Error", {
+            description: update?.message,
+          })
+        }
+      }
+      if (values.image && values.image.length) {
+        let count = 0
+        while (count < values.image.length) {
+          const uploaded = await upload.uploadFile(values.image[count])
+          if (uploaded) {
+            const update = await onUpdateGroupGallery(groupid, uploaded.uuid)
+            if (update?.status !== 200) {
+              toast("Error", {
+                description: update?.message,
+              })
+              break
+            }
+          } else {
+            toast("Error", {
+              description: "Looks like something went wrong!",
+            })
+            break
+          }
+          console.log("increment")
+          count++
+        }
+      }
+
+      return toast("Success", {
+        description: "Group gallery updated",
+      })
+    },
+  })
+
+  const onUpdateGallery = handleSubmit(async (values) => mutate(values))
+
+  return {
+    register,
+    errors,
+    onUpdateGallery,
+    isPending,
+  }
 }
--- src/hooks/payment/index.ts
+++ src/hooks/payment/index.ts
@@ -1,13 +1,16 @@
 "use client"
 
-import { onCreateNewGroup } from "@/actions/groups"
+import { onCreateNewGroup, onGetGroupChannels, onJoinGroup } from "@/actions/groups"
 import {
+  onGetActiveSubscription,
+  onGetGroupSubscriptionPaymentIntent,
   onGetStripeClientSecret,
   onTransferCommission,
 } from "@/actions/payments"
 import { CreateGroupSchema } from "@/components/forms/create-group/schema"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { loadStripe } from "@stripe/stripe-js"
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import { loadStripe, StripeCardElement } from "@stripe/stripe-js"
 import { useMutation, useQuery } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
@@ -92,3 +95,150 @@ export const usePayments = (
     creatingIntent,
   }
 }
+
+export const useActiveGroupSubscription = (groupId: string) => {
+  const { data } = useQuery({
+    queryKey: ["active-subscription"],
+    queryFn: () => onGetActiveSubscription(groupId),
+  })
+
+  return { data }
+}
+
+export const useJoinFree = (groupid: string) => {
+  const router = useRouter()
+  const onJoinFreeGroup = async () => {
+    const member = await onJoinGroup(groupid)
+    if (member?.status === 200) {
+      const channels = await onGetGroupChannels(groupid)
+      router.push(`/group/${groupid}/channel/${channels?.channels?.[0].id}`)
+    }
+  }
+
+  return { onJoinFreeGroup }
+}
+
+export const useJoinGroup = (groupid: string) => {
+  const stripe = useStripe()
+  const elements = useElements()
+
+  const router = useRouter()
+
+  const { data: Intent } = useQuery({
+    queryKey: ["group-payment-intent"],
+    queryFn: () => onGetGroupSubscriptionPaymentIntent(groupid),
+  })
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => {
+      if (!stripe || !elements || !Intent) {
+        return null
+      }
+      const { error, paymentIntent } = await stripe.confirmCardPayment(
+        Intent.secret!,
+        {
+          payment_method: {
+            card: elements.getElement(CardElement) as StripeCardElement,
+          },
+        },
+      )
+
+      if (error) {
+        console.log(error)
+        return toast("Error", {
+          description: "Oops! something went wrong, try again later",
+        })
+      }
+
+      if (paymentIntent?.status === "succeeded") {
+        const member = await onJoinGroup(groupid)
+        if (member?.status === 200) {
+          const channels = await onGetGroupChannels(groupid)
+          router.push(`/group/${groupid}/channel/${channels?.channels?.[0].id}`)
+        }
+      }
+    },
+  })
+
+  const onPayToJoin = () => mutate()
+
+  return { onPayToJoin, isPending }
+}
+
+// export const useGroupSubscription = (groupid: string) => {
+//   const {
+//     register,
+//     formState: { errors },
+//     reset,
+//     handleSubmit,
+//   } = useForm<z.infer<typeof CreateGroupSubscriptionSchema>>({
+//     resolver: zodResolver(CreateGroupSubscriptionSchema),
+//   })
+
+//   const client = useQueryClient()
+
+//   const { mutate, isPending, variables } = useMutation({
+//     mutationFn: (data: { price: string }) =>
+//       onCreateNewGroupSubscription(groupid, data.price),
+//     onMutate: () => reset(),
+//     onSuccess: (data) =>
+//       toast(data?.status === 200 ? "Success" : "Error", {
+//         description: data?.message,
+//       }),
+//     onSettled: async () => {
+//       return await client.invalidateQueries({
+//         queryKey: ["group-subscriptions"],
+//       })
+//     },
+//   })
+
+//   const onCreateNewSubscription = handleSubmit(async (values) =>
+//     mutate({ ...values }),
+//   )
+//   return { register, errors, onCreateNewSubscription, isPending, variables }
+// }
+
+// export const useAllSubscriptions = (groupid: string) => {
+//   const { data } = useQuery({
+//     queryKey: ["group-subscriptions"],
+//     queryFn: () => onGetGroupSubscriptions(groupid),
+//   })
+
+//   const client = useQueryClient()
+
+//   const { mutate } = useMutation({
+//     mutationFn: (data: { id: string }) => onActivateSubscription(data.id),
+//     onSuccess: (data) =>
+//       toast(data?.status === 200 ? "Success" : "Error", {
+//         description: data?.message,
+//       }),
+//     onSettled: async () => {
+//       return await client.invalidateQueries({
+//         queryKey: ["group-subscriptions"],
+//       })
+//     },
+//   })
+
+//   return { data, mutate }
+// }
+
+// export const useStripeConnect = (groupid: string) => {
+//   const [onStripeAccountPending, setOnStripeAccountPending] =
+//     useState<boolean>(false)
+
+//   const onStripeConnect = async () => {
+//     try {
+//       setOnStripeAccountPending(true)
+//       const account = await axios.get(`/api/stripe/connect?groupid=${groupid}`)
+//       if (account) {
+//         setOnStripeAccountPending(false)
+//         if (account) {
+//           window.location.href = account.data.url
+//         }
+//       }
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+//   return { onStripeConnect, onStripeAccountPending }
+// }
